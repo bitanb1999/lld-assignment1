d9f9dde539d7834e02d393f1705003e9191576b3
Bitan Biswas <76226078+bitanb1999@users.noreply.github.com>
2025-01-13 23:24:28 +0530

    Update Catalog.java

--- src/main/java/low_level_design_assignment/Catalog.java
+++ src/main/java/low_level_design_assignment/Catalog.java
@@ -3,28 +3,28 @@ package low_level_design_assignment;
 import java.util.*;
 
 
-class Catalog extends Search {
+class Catalog implements Search {
     private HashMap<String, List<BookItem>> bookTitles = new HashMap<>();
     private HashMap<String, List<BookItem>> bookAuthors = new HashMap<>();
     private HashMap<String, List<BookItem>> bookSubjects = new HashMap<>();
     private HashMap<String, List<BookItem>> bookPublicationDates = new HashMap<>();
 
-    @Override
+    
     public List<BookItem> searchByTitle(String query) {
         return bookTitles.getOrDefault(query, null);
     }
 
-    @Override
+    
     public List<BookItem> searchByAuthor(String query) {
         return bookAuthors.getOrDefault(query, null);
     }
 
-    @Override
+    
     public List<BookItem> searchBySubject(String query) {
         return bookSubjects.getOrDefault(query, null);
     }
 
-    @Override
+    
     public List<BookItem> searchByPubDate(String query) {
         return bookPublicationDates.getOrDefault(query, null);
     }
