9014173652f0b176555315c960ef5416da7ec705
Bitan Biswas <76226078+bitanb1999@users.noreply.github.com>
2025-01-13 23:19:48 +0530

    Update Fine.java

--- src/main/java/low_level_design_assignment/Fine.java
+++ src/main/java/low_level_design_assignment/Fine.java
@@ -4,14 +4,24 @@ public class Fine {
 private LocalDate creationDate;
 private String bookItemBarcode;
 private String memberId;
+private FineStrategy fineStrategy;
 
-public Fine(LocalDate creationDate, String bookItemBarcode, String memberId) {
+public Fine(LocalDate creationDate, String bookItemBarcode, String memberId,FineStrategy fineStrategy) {
     this.creationDate = creationDate;
     this.bookItemBarcode = bookItemBarcode;
     this.memberId = memberId;
+    this.fineStrategy = fineStrategy;
+}
+ public void setFineStrategy(FineStrategy fineStrategy) {
+    this.fineStrategy = fineStrategy;
 }
 
-public static void collectFine(String memberId, long days) {
-    // Collect fine from the member based on the number of overdue days
+public void collectFine(String memberId, long overdueDays) {
+        double fineAmount = fineStrategy.calculateFine(overdueDays);
+        System.out.println("Collected fine of $" + fineAmount + " from member " + memberId);
+        // Actual implementation to collect fine
+    }
 }
-}
+
+
+
