71d78249af5b8687dc14548cee97a1059db5b515
Bitan Biswas <76226078+bitanb1999@users.noreply.github.com>
2025-01-13 23:23:11 +0530

    Update Search.java

--- src/main/java/low_level_design_assignment/Search.java
+++ src/main/java/low_level_design_assignment/Search.java
@@ -3,13 +3,13 @@ package low_level_design_assignment;
 import java.util.HashMap;
 import java.util.List;
 
-abstract class Search {
-    public abstract List<BookItem> searchByTitle(String title);
+public interface Search {
+    public List<BookItem> searchByTitle(String title);
 
-    public abstract List<BookItem> searchByAuthor(String author);
+    public List<BookItem> searchByAuthor(String author);
 
-    public abstract List<BookItem> searchBySubject(String subject);
+    public List<BookItem> searchBySubject(String subject);
 
-    public abstract List<BookItem> searchByPubDate(String publishDate);
+    public List<BookItem> searchByPubDate(String publishDate);
 }
 
